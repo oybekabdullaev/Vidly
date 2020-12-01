ea1b50312f464440f007ab88e75a9c02fea90e70
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-01 18:14:06 +0500

    Add Create and Edit functionality to Movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -41,6 +42,56 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var oldMovie = _context.Movies.Single(m => m.Id == movie.Id);
+                oldMovie.Name = movie.Name;
+                oldMovie.ReleaseDate = movie.ReleaseDate;
+                oldMovie.NumberInStock = movie.NumberInStock;
+                oldMovie.GenreId = movie.GenreId;
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
     }
 }
