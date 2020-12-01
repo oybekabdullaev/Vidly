1cc95727c379320e917b2b8df57c3c860e6babcb
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-01 21:45:51 +0500

    Add Validation to Movies

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -50,6 +50,7 @@ namespace Vidly.Controllers
 
             var viewModel = new MovieFormViewModel()
             {
+                Movie = new Movie(),
                 Genres = genres
             };
 
@@ -57,8 +58,20 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel()
+                {
+                    Movie = movie,
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
