74987ea2a36630233b81bdabb9fbf420078de336
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-01 12:24:53 +0500

    Display Membership Type in Customers Table

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -26,7 +27,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
