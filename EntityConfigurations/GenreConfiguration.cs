a5bcba0511b53c968faba42db90eb16a88b70184
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-01 13:55:17 +0500

    Add Genre Model

---
+++ EntityConfigurations/GenreConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.EntityConfigurations
+{
+    public class GenreConfiguration : EntityTypeConfiguration<Genre>
+    {
+        public GenreConfiguration()
+        {
+            Property(g => g.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            Property(g => g.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -23,6 +23,8 @@ namespace Vidly.Models
     {
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -38,6 +40,7 @@ namespace Vidly.Models
         {
             modelBuilder.Configurations.Add(new CustomerConfiguration());
             modelBuilder.Configurations.Add(new MembershipTypeConfiguration());
+            modelBuilder.Configurations.Add(new GenreConfiguration());
 
             base.OnModelCreating(modelBuilder);
         }
