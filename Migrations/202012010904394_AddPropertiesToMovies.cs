1716260152fa8336a99510b74e9870bf1bc40a3a
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-01 14:10:11 +0500

    Add Properties to Movies

---
+++ Migrations/202012010904394_AddPropertiesToMovies.cs
@@ -0,0 +1,28 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPropertiesToMovies : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime());
+            AddColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "NumberInStock", c => c.Int(nullable: false));
+            CreateIndex("dbo.Movies", "GenreId");
+            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "GenreId" });
+            DropColumn("dbo.Movies", "NumberInStock");
+            DropColumn("dbo.Movies", "DateAdded");
+            DropColumn("dbo.Movies", "ReleaseDate");
+            DropColumn("dbo.Movies", "GenreId");
+        }
+    }
+}
--- Models/Genre.cs
+++ Models/Genre.cs
@@ -9,5 +9,6 @@ namespace Vidly.Models
     {
         public byte Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Movie> Movies { get; set; }
     }
 }
