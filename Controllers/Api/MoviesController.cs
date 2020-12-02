2eaa114cc9a1978e2f9d691de6484329f77bf8b7
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-02 17:15:17 +0500

    Restrict Movies Api to CanManageMovies Role

--- Controllers/Api/MoviesController.cs
+++ Controllers/Api/MoviesController.cs
@@ -11,6 +11,7 @@ using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
+    [Authorize(Roles = RoleName.CanManageMovies)]
     public class MoviesController : ApiController
     {
         private readonly ApplicationDbContext _context;
