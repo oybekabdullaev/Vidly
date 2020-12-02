972d233d527ddbef13668833c51bbf422bd6af99
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-02 20:41:14 +0500

    Create Rental Model

---
+++ Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Movie Movie { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateReturned { get; set; }
+    }
+}
