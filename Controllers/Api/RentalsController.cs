51445e3f451b15d65345ddf86d75af34297baa3c
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-02 20:45:23 +0500

    Create Rentals Controller with CreateRental Action

---
+++ Controllers/Api/RentalsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();;
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDto rentalDto)
+        {
+            var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            var movies = _context.Movies
+                .Where(m => rentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental()
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateCreated = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
